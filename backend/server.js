1acbdf144017f49803eadd0853a81f783a8e3822
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2022-10-23 16:54:46 +0300

    testing

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
 const app = express();
 
-console.log("Server is running!");
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
